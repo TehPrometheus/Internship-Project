f1b1c6e266e4c1f9efb763a363f4cde6162ad203
TehPrometheus <tanguy.aerts@student.howest.be>
2024-04-24 13:51:56 +0200

    Implemented thread retention

--- app5/src/app.py
+++ app5/src/app.py
@@ -1,11 +1,36 @@
 import chainlit as cl
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv("C:/Users/tangu/Documents/GitHub Repositories/Internship Project/app5/src/.env")
 
+WORKATO_API_KEY = os.getenv("WORKATO_API_KEY_CHAINLIT_CLIENT")
+
+def MakeRequest(message: cl.Message):
+    url = "https://apim.workato.com/geertv1/stage-tanguy-apis-v1/get-assistant-response"
+    headers = {
+        "api-token": WORKATO_API_KEY,
+        "Content-Type": "application/json"
+    }
+    
+    params = {
+        "thread_id": cl.user_session.get("thread_id"),
+        "content": message.content
+    }
+
+    response = requests.post(url=url, headers=headers, params=params);
+    return response.json();
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("thread_id", "")
 
 @cl.on_message
 async def HandleUserMessage(message: cl.Message):
-    # Your custom logic goes here...
+    response = MakeRequest(message);
+    print(f"\n>>>> got the Workato API response as: \n{response}");
+    cl.user_session.set("thread_id", response["thread_id"]);
 
     # Send a response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+    await cl.Message(content = response['message']).send()
+ 
