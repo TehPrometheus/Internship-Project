f0274aba682d1a9bcec2da7b0ca160d1e743be1c
TehPrometheus <tanguy.aerts@student.howest.be>
2024-05-21 15:31:02 +0200

    implemented basic streaming

--- Kaya/src/app.py
+++ Kaya/src/app.py
@@ -1,6 +1,5 @@
 import chainlit as cl
 from kaya import Kaya
-from kaya import EventHandler
 
 # Initialize Kaya
 kaya = Kaya()
@@ -19,18 +18,12 @@ async def on_message(message: cl.Message):
     msg = cl.Message(content="")
     await msg.send()
     
-    # Fetch Kaya's response to the user message
-    # kaya_response = kaya.get_response(message.content)
+    # Add the user message to the thread
+    kaya.add_message_to_thread(message.content)
 
+    # Run the thread and stream the response
+    kaya.run_thread_and_stream()
 
-    # Update the empy msg object with Kaya's response
-    # msg.content = kaya_response
-
-    text_delta = kaya.PassDeltaToChainlit();
-    for token in text_delta:
-        await msg.stream_token(token)
-
-    await msg.send()
 
 
 
